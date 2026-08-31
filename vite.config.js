8bb00e701b17857e98bb79d09ba02e3067cc2391
youssefbg1 <benghorbelyoussef3@gmail.com>
2026-08-31 18:26:28 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react()],
-    base : '/portfolio/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
